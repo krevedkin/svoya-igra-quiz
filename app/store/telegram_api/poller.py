1f7a5066b50c5521bddd31b18f8d001bfe678362
igor <frakestolstm@gmail.com>
2023-02-28 17:13:29 +0400

    added logger, added try-except to poll method

--- app/store/telegram_api/poller.py
+++ app/store/telegram_api/poller.py
@@ -1,16 +1,18 @@
 import asyncio
 from asyncio import Task
+from logging import getLogger
+from pprint import pformat
 from typing import Optional
 
 from app.store import Store
 
 
 class Poller:
-
     def __init__(self, store: Store):
         self.store = store
         self.is_running = False
         self.poll_task: Optional[Task] = None
+        self.logger = getLogger("Poller")
 
     async def start(self):
         self.is_running = True
@@ -21,9 +23,10 @@ class Poller:
         await self.poll_task
 
     async def poll(self):
-
         while self.is_running:
-            print("начал поллинг!")
-            # updates = await self.store.vk_api.poll()
-            updates = await self.store.tg_api.poll()
-            await self.store.bots_manager.handle_updates(updates)
+            try:
+                updates = await self.store.tg_api.poll()
+                await self.store.bots_manager.handle_updates(updates)
+            except Exception as e:
+                self.logger.exception(e)
+                self.logger.error(pformat(updates))

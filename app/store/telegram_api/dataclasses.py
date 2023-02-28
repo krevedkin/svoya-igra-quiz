2b3320c5bd03c76988e73d02928f39e70682a19e
igor <frakestolstm@gmail.com>
2023-02-28 17:11:38 +0400

    refactored Message, CallbackQuery, Update. Added new dataclasses

--- app/store/telegram_api/dataclasses.py
+++ app/store/telegram_api/dataclasses.py
@@ -1,30 +1,87 @@
 from dataclasses import dataclass
-from datetime import datetime
+
+
+@dataclass
+class User:
+    id: int
+    is_bot: bool
+    first_name: str
+    last_name: str | None = None
+    username: str | None = None
+
+
+@dataclass
+class PollOption:
+    text: str
+    voter_count: int
+
+
+@dataclass
+class PollAnswer:
+    poll_id: int
+    user: User
+    option_ids: list[int]
+
+
+@dataclass
+class Poll:
+    id: int
+    question: str
+    options: list[PollOption]
+    total_voter_count: int
+    is_closed: bool
+    is_anonymous: bool
+    type: bool
+    allows_multiple_answers: bool
+
+
+@dataclass
+class Chat:
+    id: int
+    type: str
+    username: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+
+
+@dataclass
+class MessageEntity:
+    type: str
+    offset: int
+    length: int
 
 
 @dataclass
 class Message:
-    message_id: int | None = None
-    chat_id: int | None = None
-    username: str | None = None
-    is_bot: bool | None = None
-    date: datetime | int | None = None
+    message_id: int
+    date: int
+    chat: Chat
+    from_: User | None = None
     text: str | None = None
-    type_: str = "text"
-
-    def __post_init__(self):
-        if self.date:
-            self.date = datetime.fromtimestamp(self.date)
+    entities: list[MessageEntity] | None = None
+    poll: Poll | None = None
 
 
 @dataclass
 class CallbackQuery:
-    id: str
-    data: str
+    id: int
+    from_: User
+    message: Message
+    inline_message_id: str | None = None
+    chat_instance: str | None = None
+    data: str | None = None
 
 
 @dataclass
-class UpdateObject:
+class Update:
     update_id: int
-    message: Message
+    message: Message | None = None
+    poll: Poll | None = None
+    poll_answer: PollAnswer | None = None
     callback_query: CallbackQuery | None = None
+
+
+@dataclass
+class ChatMemberAdministrator:
+    status: str
+    user: User

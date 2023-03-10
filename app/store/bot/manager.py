73696b3a6cd6da30bba52dfdb405828e9953b822
igor <frakestolstm@gmail.com>
2023-03-10 04:13:24 +0400

    refacotred handle_updates

--- app/store/bot/manager.py
+++ app/store/bot/manager.py
@@ -1,7 +1,7 @@
 import typing
 from logging import getLogger
 from pprint import pformat
-
+from icecream import ic
 from app.store.bot.bot import Bot
 from app.store.telegram_api.dataclasses import Update, Message
 
@@ -17,24 +17,28 @@ class BotManager:
 
     async def handle_updates(self, updates: list[Update]):
         for update in updates:
-            self.logger.info(pformat(update))
-            self.logger.info("---------------------------------------")
+            # self.logger.info(pformat(update))
+            # self.logger.info("---------------------------------------")
+            ic(update)
             self.bot.update = update
             if update.callback_query:
-                await self.bot.callback_query_handler()
+                # await self.bot.callback_query_handler()
+                await self.bot.handle_callback_query()
 
             elif update.message and update.message.entities:
-                await self.bot.command_handler()
+                # await self.bot.command_handler()
+                await self.bot.handle_command()
 
-            elif update.message and update.message.new_chat_member:
-                await self.bot.new_chat_user_handler()
-
-            elif update.message and update.message.left_chat_member:
-                await self.bot.left_chat_user_handler()
+            # elif update.message and update.message.new_chat_member:
+            #     await self.bot.new_chat_user_handler()
+            #
+            # elif update.message and update.message.left_chat_member:
+            #     await self.bot.left_chat_user_handler()
 
             elif update.message:
-                game = await self.app.store.game.get_game_by_chat_id(
-                    update.message.chat.id
-                )
-                if game:
-                    await self.bot.answer_message_handler()
+                await self.bot.handle_answer_message()
+                # game = await self.app.store.game.get_game_by_chat_id(
+                #     update.message.chat.id
+                # )
+                # if game:
+                #     await self.bot.answer_message_handler()

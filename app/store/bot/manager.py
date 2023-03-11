a77a0de5866ec37f950ae7af81a9ea2c85119f14
igor <frakestolstm@gmail.com>
2023-03-12 02:16:31 +0400

    refactored handle_updates

--- app/store/bot/manager.py
+++ app/store/bot/manager.py
@@ -1,9 +1,7 @@
 import typing
 from logging import getLogger
-from pprint import pformat
-from icecream import ic
 from app.store.bot.bot import Bot
-from app.store.telegram_api.dataclasses import Update, Message
+from app.store.telegram_api.dataclasses import Update
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -17,28 +15,12 @@ class BotManager:
 
     async def handle_updates(self, updates: list[Update]):
         for update in updates:
-            # self.logger.info(pformat(update))
-            # self.logger.info("---------------------------------------")
-            ic(update)
             self.bot.update = update
             if update.callback_query:
-                # await self.bot.callback_query_handler()
                 await self.bot.handle_callback_query()
 
             elif update.message and update.message.entities:
-                # await self.bot.command_handler()
                 await self.bot.handle_command()
-
-            # elif update.message and update.message.new_chat_member:
-            #     await self.bot.new_chat_user_handler()
-            #
-            # elif update.message and update.message.left_chat_member:
-            #     await self.bot.left_chat_user_handler()
 
             elif update.message:
                 await self.bot.handle_answer_message()
-                # game = await self.app.store.game.get_game_by_chat_id(
-                #     update.message.chat.id
-                # )
-                # if game:
-                #     await self.bot.answer_message_handler()
